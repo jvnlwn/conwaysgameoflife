6a3be8e7c6e09a1863968c3f39913f498424bed3
Joe VanLeeuwen <jvnlwn@gmail.com>
2014-03-27 16:48:12 -0400

    all set

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+// Each celll is in one of two possible states, alive or dead.
+
+// The following transitions occur:
+
+// 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+// 2. Any live cell with two or three live neighbours lives on to the next generation.
+// 3. Any live cell with more than three live neighbours dies, as if by overcrowding.
+// 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+// A tick is the transition from one generation to the next.
+
+console.log('yo')
+
+
